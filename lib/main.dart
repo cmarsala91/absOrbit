8a112b45e9bfaaccdc9a1a5eca8f086917175d38
cmarsala <cmarsala91@gmail.com>
2020-10-02 19:53:09 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,87 +1,109 @@
-import 'dart:math';
-
-import 'package:absorbit/pie.dart';
-import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
 
-import 'bar.dart';
-
 void main() {
-  runApp(MaterialApp(home: ChartPage()));
+  runApp(MaterialApp(
+    title: 'Flutter Tutorial',
+    home: TutorialHome(),
+  ));
 }
 
-class ChartPage extends StatefulWidget {
+class TutorialHome extends StatelessWidget {
   @override
-  ChartPageState createState() => ChartPageState();
+  Widget build(BuildContext context) {
+    // Scaffold is a layout for the major Material Components.
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          tooltip: 'Navigation menu',
+          onPressed: null,
+        ),
+        title: Text('Example title'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: null,
+          ),
+        ],
+      ),
+      // body is the majority of the screen.
+      body: Center(
+        child: Text('Hello, world!'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Add', // used by assistive technologies
+        child: Icon(Icons.add),
+        onPressed: null,
+      ),
+    );
+  }
 }
 
-class ChartPageState extends State<ChartPage> with TickerProviderStateMixin {
-  static const size = const Size(200.0, 100.0);
-  final random = Random();
-  AnimationController animation;
-  BarChartTween tween;
-  PieChartTween pieTween;
+class MyButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        print('MyButton was tapped!');
+      },
+      child: Container(
+        height: 36.0,
+        padding: const EdgeInsets.all(8.0),
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5.0),
+          color: Colors.lightGreen[500],
+        ),
+        child: Center(
+          child: Text('Engage'),
+        ),
+      ),
+    );
+  }
+}
 
+class Counter extends StatefulWidget {
+  // This class is the configuration for the state. It holds the
+  // values (in this case nothing) provided by the parent and used
+  // by the build  method of the State. Fields in a Widget
+  // subclass are always marked "final".
 
   @override
-  void initState() {
-    super.initState();
-    animation = AnimationController(
-      duration: const Duration(milliseconds: 300),
-      vsync: this,
-    );
-    tween = BarChartTween(
-      BarChart.empty(size),
-      BarChart.random(size, random),
-    );
-    pieTween = PieChartTween(
-      PieChart.empty(size),
-      PieChart.random(size, random),
-    );
-    animation.forward();
-  }
+  _CounterState createState() => _CounterState();
+}
 
-  @override
-  void dispose() {
-    animation.dispose();
-    super.dispose();
-  }
+class _CounterState extends State<Counter> {
+  int _counter = 0;
 
-  void changeData() {
+  void _increment() {
     setState(() {
-      tween = BarChartTween(
-        tween.evaluate(animation),
-        BarChart.random(size, random),
-      );
-      pieTween = PieChartTween(
-        pieTween.evaluate(animation),
-        PieChart.random(size, random),
-      );
-      animation.forward(from: 0.0);
+      // This call to setState tells the Flutter framework that
+      // something has changed in this State, which causes it to rerun
+      // the build method below so that the display can reflect the
+      // updated values. If you change _counter without calling
+      // setState(), then the build method won't be called again,
+      // and so nothing would appear to happen.
+      _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Row(
-          children: [
-            CustomPaint(
-              size: size,
-              painter: PieChartPainter(pieTween.animate(animation)),
-            ),
-            CustomPaint(
-              size: size,
-              painter: BarChartPainter(tween.animate(animation)),
-            ),
-          ],
+    // This method is rerun every time setState is called,
+    // for instance, as done by the _increment method above.
+    // The Flutter framework has been optimized to make rerunning
+    // build methods fast, so that you can just rebuild anything that
+    // needs updating rather than having to individually change
+    // instances of widgets.
+    return Row(
+      children: <Widget>[
+        RaisedButton(
+          onPressed: _increment,
+          child: Text('Increment'),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.refresh),
-        onPressed: changeData,
-      ),
+        Text('Count: $_counter'),
+      ],
     );
   }
 }
